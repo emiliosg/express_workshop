48c2c71cbacf5b7dc72a83525fe5548a98e1281e
emiliosg <3miliosanchez8@gmail.com>
2021-04-23 11:58:14 -0500

    Adding user login with token

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -21,6 +22,35 @@ const { user_name, user_mail, user_password } = req.body;
 	return res.status(500).json({ code: 500, message: "Ocurrio un error"});
 	}
 	return res.status(500).json({ code: 500, message: "Campos incompletos"});
+});
+
+user.post("/login",async (req,res,next)=>{
+	//no importa el orden de las variables
+	const {user_mail,user_password} = req.body;
+	const query =  `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`
+	rows = await db.query(query);
+
+	if(user_mail && user_password){
+	
+		if(rows.length == 1)
+		{
+			const token = jwt.sign({
+				user_id: rows[0].user_id,
+				user_mail: rows[0].user_mail
+			},"debugkey");
+			return res.status(200).json({code:200,message:token});
+		}
+		else{
+
+			return res.status(200).json({code:200,message:"Usuario y/o contraseña incorrectos"});
+		}
+	}
+	else{
+
+		return res.status(500).json({code:500,message:"Campos incompletos"});
+	}
+
+
 });
 
 user.get("/",async(req,res,next) => {
